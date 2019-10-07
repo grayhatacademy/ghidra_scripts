0a1b2688d6f486eb061c4b6fbe3ca222935fe0d9
fuzzywalls <39141855+fuzzywalls@users.noreply.github.com>
2019-10-07 11:41:31 -0400

    Fixed crash in rename variables when getSymbol returned None.

--- RenameVariables.py
+++ RenameVariables.py
@@ -1,7 +1,7 @@
-# Rename saved stack variables in MIPS programs. 
+# Rename saved stack variables in MIPS programs.
 #@author fuzzywalls
 #@category TNS
-#@keybinding 
+#@keybinding
 #@menupath TNS.Rename Variables
 
 from ghidra.program.model.symbol import SourceType
@@ -14,15 +14,19 @@ func_list = func_man.getFunctions(True)
 
 for func in func_list:
     # Order of this list is important.
-    savable_registers = ['gp', 's0', 's1', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 'ra']
+    savable_registers = ['gp', 's0', 's1', 's2',
+                         's3', 's4', 's5', 's6', 's7', 's8', 'ra']
 
     variables = func.getAllVariables()
     for var in variables:
         if len(savable_registers) == 0:
             break
-        
+
         symbol = var.getSymbol()
-        references = var.getSymbol().getReferences()
+        if not symbol:
+            continue
+
+        references = symbol.getReferences()
 
         for ref in references:
             ins = code_man.getInstructionAt(ref.getFromAddress())
@@ -32,8 +36,10 @@ for func in func_list:
                 if saved_register:
                     saved_register = saved_register.toString()
                     if saved_register in savable_registers:
-                        var.setName('saved_%s' % saved_register, SourceType.USER_DEFINED)
-                        
-                        # Remove the saved register to avoid renaming registers saved later.
+                        var.setName('saved_%s' % saved_register,
+                                    SourceType.USER_DEFINED)
+
+                        # Remove the saved register to avoid renaming registers
+                        # saved later.
                         index = savable_registers.index(saved_register)
                         savable_registers = savable_registers[:index]
